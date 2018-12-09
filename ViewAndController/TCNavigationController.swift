3195a92bfa579c5aca1463acd10469fd92a4af53
PalmaRandolph <44799670+PalmaRandolph@users.noreply.github.com>
2018-12-09 20:08:18 +0800

    Add files via upload

---
+++ ViewAndController/TCNavigationController.swift
@@ -0,0 +1,18 @@
+
+
+import UIKit
+
+class TCNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
